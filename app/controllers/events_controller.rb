8805d3badf8b0016f53ba2ede9855db4c9473d98
Kevin <cho.kevin@hotmail.com>
2017-08-28 12:54:51 +0800

    added create action to api

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,10 +7,22 @@ class EventsController < ApplicationController
   def show
     render json: @event
   end
+  def create
+    @event= Event.new(event_params)
+    if @event.save
+      render json: @event, status: :created, location: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
 
   private
     def set_event
       @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit!
     end
 
 
