51fb0d94b00e8d7e10b05fccd67b74e261962006
Kevin <cho.kevin@hotmail.com>
2017-09-04 19:19:46 +0800

    detail controller and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
   resources :users, only: [:index, :update]
-  resources :user_details
+  resources :details
 end
